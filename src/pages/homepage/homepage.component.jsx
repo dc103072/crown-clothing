893e855ad2852be8b8f85a9f15b8feb21431d932
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-07-10 19:17:02 -0500

    Load initial files

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+
 const HomePage = () => (
 <div className ='homepage'>
 <div className='directory-menu'>
