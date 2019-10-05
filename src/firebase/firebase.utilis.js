90d2013ef6da94b10d99906c6feeaedeb2debeb6
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-10-05 14:24:15 -0500

    lesson 25

--- src/firebase/firebase.utilis.js
+++ src/firebase/firebase.utilis.js
@@ -6,6 +6,7 @@ import 'firebase/auth';
 const config = {
     apiKey: "AIzaSyB4kz09s-QECCZIFyCk_pbAGZGaft3TON8",
     authDomain: "crwn-db-2686f.firebaseapp.com",
+    //authDomain: "https://crowndc-live.herokuapp.com",
     databaseURL: "https://crwn-db-2686f.firebaseio.com",
     projectId: "crwn-db-2686f",
     storageBucket: "",
