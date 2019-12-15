bcf3d135b74d0c7578c89946731b3ac6fb790da9
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-12-15 10:06:37 -0600

    lesson 26 with header and custom button using styled components

--- src/pages/homepage/homepage.component - Copy.jsx
+++ src/pages/homepage/homepage.component - Copy.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-//import './homepage.styles.scss';
+import './homepage.styles.scss';
 
 import Directory from '../../components/directory/directory.component';
-import { HomePageContainer  } from './homepage.styles';
-
-
 
 const HomePage = () => (
-//<div className ='homepage'>
-<HomePageContainer>
+<div className ='homepage'>
 <Directory />
-</HomePageContainer>
+</div>
 	);
 
 export default HomePage;
