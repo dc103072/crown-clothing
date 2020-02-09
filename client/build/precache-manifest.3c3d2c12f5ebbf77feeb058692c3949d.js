c9ab89bc5a23fc6d165d7b67add922a10c791db8
dc103072 <47541752+dc103072@users.noreply.github.com>
2020-02-09 07:54:02 -0600

    lesson 31

---
+++ client/build/precache-manifest.3c3d2c12f5ebbf77feeb058692c3949d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd39acd489b8b608b714aced6306a4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c8eb5b73accda1f252",
+    "url": "/static/css/main.3193f861.chunk.css"
+  },
+  {
+    "revision": "60164c7144df1fd3380e",
+    "url": "/static/js/2.6f24fc3d.chunk.js"
+  },
+  {
+    "revision": "63e3812084deead4b9cceb4ebd8128cc",
+    "url": "/static/js/2.6f24fc3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03c8eb5b73accda1f252",
+    "url": "/static/js/main.2d703b74.chunk.js"
+  },
+  {
+    "revision": "d741a0a1132ffb92351c",
+    "url": "/static/js/runtime-main.62a8079c.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "bffe4871e561bc6d63a459e0660b26b7",
+    "url": "/static/media/shopping-bag.bffe4871.svg"
+  }
+]);
